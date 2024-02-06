f0fca09c7fd915db0c6c87b92a11ee5a4e902def
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-06 10:26:44 -0300

    feat: validation and create new poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,27 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { z } from 'zod';
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 app.get('/hello', () => 'Hello world')
+
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPollBody.parse(request.body)
+
+  const { id } = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  reply.status(201).send({ id: id })
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server listening on port 3333')
