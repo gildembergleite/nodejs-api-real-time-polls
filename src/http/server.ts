7f88a9e9aa6d3ab4b2f40f967d8d51e41ed91ded
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-06 22:21:07 -0300

    feat: create poll options

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,10 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
-import { z } from 'zod';
+import { createPoll } from './routes/create-poll';
+
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/hello', () => 'Hello world')
-
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const { id } = await prisma.poll.create({
-    data: {
-      title
-    }
-  })
-
-  reply.status(201).send({ id: id })
-})
+app.register(createPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server listening on port 3333')
