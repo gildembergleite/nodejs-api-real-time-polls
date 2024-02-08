d7e8323b30f3891baa148bb032fe452b548168e7
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-08 12:10:47 -0300

    feat: increment and decrement votes in redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'node:crypto';
 import { z } from 'zod';
 import { prisma } from '../../lib/prisma';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -33,6 +34,8 @@ export async function voteOnPoll(app: FastifyInstance) {
       await prisma.vote.delete({
         where: { id: userPreviousVoteOnPoll.id }
       })
+
+      await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.id)
     } else if (userPreviousVoteOnPoll) {
       return reply.status(400).send({
         message: 'You already voted on this poll'
@@ -58,6 +61,8 @@ export async function voteOnPoll(app: FastifyInstance) {
       pollOptionId
     }
   })
+    
+  await redis.zincrby(pollId, 1, pollOptionId)
 
   reply.status(201).send()
 })
